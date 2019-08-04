aa6751610182dce08a2634ede6eec4cda535884d
Shadow7101 <shadow7001@outlook.com>
2019-08-04 00:38:30 -0300

    arquivos da seção 4

--- ProAgilApp/src/app/eventos/eventos.component.ts
+++ ProAgilApp/src/app/eventos/eventos.component.ts
@@ -7,22 +7,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista) : this.eventos;
+  }
 
-  eventos: any;
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { this.getEventos(); }
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  filtrarEvento(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
 
 
   getEventos() {
     this.http.get('http://localhost:5000/api/values')
       .subscribe(response => {
         this.eventos = response;
-        console.log(response);
+        this.eventosFiltrados = response;
       }, error => {
         console.log(error);
       });
+  }
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
 }
